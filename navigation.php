3f5c0b22b3503e9714c7b91de8e653f42738c81f
Carole Tierney <59835292+caroleatierney@users.noreply.github.com>
2020-02-05 08:21:57 -0500

    Add files via upload
    Files before responsive coding added

---
+++ navigation.php
@@ -0,0 +1,16 @@
+<head>
+	<LINK rel="stylesheet" type="text/css" href="style.css" title="Styles" media="screen">
+</head>
+
+<ul class="sidenav">
+  	<li><img src="images/namelogo.jpg" width="100%" alt="Maggie's Logo"></li>
+    <li><a href="index.php">HOME</a></li>
+	<li><a href="services.php"> SERVICE LIST</a></li>
+	<li><a href="hours.php">HOURS</a></li>
+	<li><a href="appointment.php">APPOINTMENTS</a></li>
+	<li><a href="contact.php">CONTACT ME</a></li>
+	<li><a href="about.php">ABOUT ME</a></li>
+	<li><a href="client.php">CLIENT BEFORE/AFTER</a></li>
+	<li><a href="testimonials.php">TESTIMONIALS</a></li>
+	<li><a href="gallery.php">GALLERY</a></li>
+</ul>
---
+++ testimonials.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  <title>Testimonials</title>
+  <link href="/normalize.css" rel="stylesheet">
+  <link rel="stylesheet" href="style.css"/>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Side navigation styling -->
+<?php include_once 'stylemenu.css';?> 	
+
+</head>
+
+<body>
+
+<!-- Side navigation menu list -->
+<?php include_once 'navigation.php';?>
+
+<div class="testimonials">
+	<h1>Testimonials</h1>
+  	<p><b>October 21, 2019</b>: Fantastic hair cut from the supremely talented Maggie.</p>
+	<p><b>November 2, 2019</b>: Maggie is the best!  she is very professional and does excellent quality work.  Her new shop is top of the line,  its bright, clean and very modern!</p>
+	<p><b>December 7, 2019</b>: Maggie did an amazing job trimming my boyfriend's beard and giving him a well blended haircut that hid his crazy cowlick. I highly recommend her!</p>
+</div>
+
+</body>
+</html>
